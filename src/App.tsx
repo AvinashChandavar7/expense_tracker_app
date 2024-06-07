a52bd63b1b31045afbf70cdd8fa01b184c030cdf
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-06-07 15:33:41 +0530

    feat : add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Heading from './components/ui/Heading/Heading'
+import { Button } from './components/ui/Button/Button'
+import { FaPlus } from 'react-icons/fa'
 
 const App: React.FC = () => {
   return (
     <div>
       <Heading title="Expense Tracker" />
+      <Button text='Add Income' className='btn_primary--green' icon={<FaPlus className="icon" size={10} />} />
+      <Button text='Add Expense' className='btn_primary--red' icon={<FaPlus className="icon" size={10} />} />
+      <Button text='Add Expense' className='btn_primary--yellowishOrange' icon={<FaPlus className="icon" size={10} />} />
     </div>
   )
 }
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import clsx from "clsx"
+import "./Button.css"
+
+
+// Define your button variants
+const buttonVariants = {
+  primary: "btn_primary",
+}
+
+// Utility type to get the keys of buttonVariants
+type VariantProps<T> = {
+  variant?: keyof T;
+}
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  VariantProps<typeof buttonVariants> {
+  text: string;
+  icon?: React.ReactNode;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant = "primary", text, icon, ...props }, ref) => {
+    return (
+      <button
+        className={clsx(buttonVariants[variant], className)}
+        ref={ref}
+        {...props} >
+        {icon && <span className="icon">{icon}</span>}
+        {text}
+      </button >
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button }
